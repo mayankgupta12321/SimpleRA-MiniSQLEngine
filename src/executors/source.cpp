4273ebed419f96fe1daf641c2f2a9d10d29c9904
Ardhendu Banerjee <ardhendubanerjee@Ban.local>
2023-08-17 10:09:07 +0530

    17th Aug Class Activity 1

--- src/executors/source.cpp
+++ src/executors/source.cpp
@@ -1,4 +1,5 @@
 #include "global.h"
+#include <fstream>
 /**
  * @brief 
  * SYNTAX: SOURCE filename
@@ -19,6 +20,7 @@ bool syntacticParseSOURCE()
 bool semanticParseSOURCE()
 {
     logger.log("semanticParseSOURCE");
+    // cout<<"File Name: "<<parsedQuery.sourceFileName<<'\n';
     if (!isQueryFile(parsedQuery.sourceFileName))
     {
         cout << "SEMANTIC ERROR: File doesn't exist" << endl;
@@ -27,8 +29,60 @@ bool semanticParseSOURCE()
     return true;
 }
 
+void SubDoCommand()
+{
+    logger.log("doCommand");
+    if (syntacticParse() && semanticParse())
+        executeCommand();
+    return;
+}
+
 void executeSOURCE()
 {
+    regex delim("[^\\s,]+");
+    string command;
+    system("rm -rf ../data/temp");
+    system("mkdir ../data/temp");
+
+
     logger.log("executeSOURCE");
+    ifstream fin;
+    fin.open("../data/" + tokenizedQuery[1] + ".ra");
+    while (getline(fin, command)) {
+        cout<<"Command Executing : "<<command<<'\n';
+        cout<<'\n';
+        tokenizedQuery.clear();
+        parsedQuery.clear();
+        logger.log("\nReading New Command: ");
+        // getline(cin, command);
+        logger.log(command);
+
+
+        auto words_begin = std::sregex_iterator(command.begin(), command.end(), delim);
+        auto words_end = std::sregex_iterator();
+        for (std::sregex_iterator i = words_begin; i != words_end; ++i)
+            tokenizedQuery.emplace_back((*i).str());
+
+        if (tokenizedQuery.size() == 1 && tokenizedQuery.front() == "QUIT")
+        {
+            break;
+        }
+
+        if (tokenizedQuery.empty())
+        {
+            continue;
+        }
+
+        if (tokenizedQuery.size() == 1)
+        {
+            cout << "SYNTAX ERROR" << endl;
+            continue;
+        }
+
+        SubDoCommand();
+        // cout << line << endl;
+    }
+ 
+    fin.close();
     return;
 }
